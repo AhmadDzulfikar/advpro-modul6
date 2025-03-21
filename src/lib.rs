4ad6a6f991d5dd258c585a92f434a21bddbc8042
AhmadDzulfikar <dzulfikar19.ahmad@gmail.com>
2025-03-21 17:12:31 +0700

    (Bonus) Function Improvement

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,24 @@
-use std::{
-    sync::{mpsc, Arc, Mutex},
+use std::{ 
+    sync::{mpsc, Arc, Mutex}, 
     thread,
+    error::Error,
+    fmt,
 };
+
+#[derive(Debug)]
+pub enum PoolCreationError {
+    ZeroSize,
+}
+
+impl fmt::Display for PoolCreationError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            PoolCreationError::ZeroSize => write!(f, "Thread pool size must be greater than zero"),
+        }
+    }
+}
+
+impl Error for PoolCreationError {}
 
 pub struct ThreadPool {
     workers: Vec<Worker>,
@@ -11,28 +28,26 @@ pub struct ThreadPool {
 type Job = Box<dyn FnOnce() + Send + 'static>;
 
 impl ThreadPool {
-    pub fn new(size: usize) -> ThreadPool {
-        assert!(size > 0);
+    pub fn build(size: usize) -> Result<ThreadPool, PoolCreationError> {
+        if size == 0 {
+            return Err(PoolCreationError::ZeroSize);
+        }
 
         let (sender, receiver) = mpsc::channel();
-
         let receiver = Arc::new(Mutex::new(receiver));
-
         let mut workers = Vec::with_capacity(size);
 
         for id in 0..size {
             workers.push(Worker::new(id, Arc::clone(&receiver)));
         }
 
-        ThreadPool { workers, sender }
+        Ok(ThreadPool { workers, sender })
     }
 
     pub fn execute<F>(&self, f: F)
-    where
         F: FnOnce() + Send + 'static,
     {
         let job = Box::new(f);
-
         self.sender.send(job).unwrap();
     }
 }
@@ -47,7 +62,6 @@ impl Worker {
         let thread = thread::spawn(move || {
             while let Ok(job) = receiver.lock().unwrap().recv() {
                 println!("Worker {id} got a job; executing.");
-
                 job();
             }
         });
